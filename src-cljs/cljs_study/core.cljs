f05ef4785c7c43cdef7521c248a1e21a976fe97f
Masao KATO <ponkore@gmail.com>
2014-07-23 13:39:58 +0900

    modify indent (my favorite style)

--- src-cljs/cljs_study/core.cljs
+++ src-cljs/cljs_study/core.cljs
@@ -4,68 +4,77 @@
 
 (def state (atom {:doc {} :saved? false}))
 
-(defn set-value! [id value]
-      (swap! state assoc :saved? false)
-      (swap! state assoc-in [:doc id] value))
+(defn set-value!
+  [id value]
+  (swap! state assoc :saved? false)
+  (swap! state assoc-in [:doc id] value))
 
-(defn get-value [id]
-      (get-in @state [:doc id]))
+(defn get-value
+  [id]
+  (get-in @state [:doc id]))
 
-(defn row [label & body]
-      [:div.row
-       [:div.col-md-2 [:span label]]
-       [:div.col-md-3 body]])
+(defn row
+  [label & body]
+  [:div.row
+   [:div.col-md-2 [:span label]]
+   [:div.col-md-3 body]])
 
-(defn text-input [id label]
+(defn text-input
+  [id label]
+  [row label
+   [:input {:type "text"
+            :class "form-control"
+            :value (get-value id)
+            :onChange #(set-value! id (-> % .-target .-value))}]])
+
+(defn list-item
+  [id k v states]
+  (letfn [(handle-click! []
+            (swap! (get states k) not)
+            (set-value! id (->> states (filter (fn [[_ v]] @v)) keys)))]
+    [:li {:class (str "list-group-item" (if @(get states k) " active"))
+          :onClick handle-click!}
+     v]))
+
+(defn selection-list
+  [id label & items]
+  (let [states (->> items
+                    (map (fn [[k]] [k (atom false)]))
+                    (into {}))]
+    (fn []
       [row label
-       [:input {:type "text"
-                :class "form-control"
-                :value (get-value id)
-                :onChange #(set-value! id (-> % .-target .-value))}]])
+       [:ul.list-group
+        (for [[k v] items]
+          [list-item id k v states])]])))
 
-(defn list-item [id k v states]
-      (letfn [(handle-click! []
-                             (swap! (get states k) not)
-                             (set-value! id (->> states (filter (fn [[_ v]] @v)) keys)))]
-             [:li {:class (str "list-group-item" (if @(get states k) " active"))
-                   :onClick handle-click!}
-              v]))
+(defn save-doc
+  []
+  (POST (str js/context "/save")
+      {:params (:doc @state)
+       :handler (fn [_] (swap! state assoc :saved? true))}))
 
-(defn selection-list [id label & items]
-      (let [states (->> items
-                        (map (fn [[k]] [k (atom false)]))
-                        (into {}))]
-           (fn []
-               [row label
-                [:ul.list-group
-                 (for [[k v] items]
-                      [list-item id k v states])]])))
+(defn err
+  []
+  (if-let [saved (:saved? @state)]
+    [:p "HOGE"]))
 
-(defn save-doc []
-      (POST (str js/context "/save")
-            {:params (:doc @state)
-             :handler (fn [_] (swap! state assoc :saved? true))}))
+(defn home
+  []
+  [:div
+   [:div.page-header [:h1 "Reagent Form"]]
 
-(defn err []
-      (if-let [saved (:saved? @state)]
-              [:p "HOGE"]))
+   [err]
+   [text-input :first-name "First name"]
+   [text-input :last-name "Last name"]
+   [selection-list :favorite-drinks "Favorite drinks"
+    [:coffee "Coffee"] [:beer "Beer"] [:crab-juice "Crab juice"]]
 
-(defn home []
-      [:div
-       [:div.page-header [:h1 "Reagent Form"]]
-
-       [err]
-       [text-input :first-name "First name"]
-       [text-input :last-name "Last name"]
-       [selection-list :favorite-drinks "Favorite drinks"
-        [:coffee "Coffee"] [:beer "Beer"] [:crab-juice "Crab juice"]]
-
-       (if (:saved? @state)
-         [:p "Saved"]
-         [:button {:type "submit"
-                   :class "btn btn-default"
-                   :onClick save-doc}
-          "Submit"])])
+   (if (:saved? @state)
+     [:p "Saved"]
+     [:button {:type "submit"
+               :class "btn btn-default"
+               :onClick save-doc}
+      "Submit"])])
 
 ;;start the app
 (reagent/render-component [home] (.getElementById js/document "app"))
